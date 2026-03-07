2f1be87bad6518abef3afe5eee47ddba8e6aed21
mayconcoutinhodev <mayconcoutinhodev@gmail.com>
2026-03-06 22:11:08 -0300

    ci: adiciona prisma generate e envs para o next-auth

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,9 +30,9 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: 'npm run dev',
+    command: 'npm run build && npm run start',
     url: 'http://127.0.0.1:3000',
     reuseExistingServer: !process.env.CI,
-    timeout: 120 * 1000,
+    timeout: 180 * 1000,
   },
 });
